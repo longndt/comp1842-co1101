a8b7d7f798599736992c120c55be6fdc8b5b5d80
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-05-15 10:35:24 +0700

    week1

---
+++ week1/demo.js
@@ -0,0 +1,12 @@
+
+   function welcome() {
+      alert("Welcome to module COMP1842");
+      }
+
+   function changeColor() {
+      document.getElementById("web2").style.color = "blue";
+      }
+
+   function changeText() {
+      document.getElementById("web2").innerHTML = "COMP1842";
+      }
