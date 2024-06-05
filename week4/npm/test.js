63a50b65c071dba81e27c48759384fa14adad4cd
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-06-05 12:14:43 +0700

    week4

---
+++ week4/npm/test.js
@@ -0,0 +1,11 @@
+/* display text to console */
+console.log("Web Programming 2 - COMP1842")
+console.log("Heavy rain today")
+
+/* declare an array */
+const sports = ["football", "badminton", "swimming"]
+
+/* use foreach loop to display all elements in this array */
+sports.forEach(s => {
+   console.log(s)
+})
